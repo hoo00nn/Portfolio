a07fc63e0591fa474186174b18bb41f00b821123
hoo00nn <hoo00nn@naver.com>
2020-06-11 11:41:14 +0900

    JS : Navbar

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// 스크롤 됐을 경우 Navbar Style 변경
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
