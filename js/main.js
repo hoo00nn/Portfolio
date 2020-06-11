cfeb6f8f8a5333a9a0dca19fb41dea85f3cee16a
hoo00nn <hoo00nn@naver.com>
2020-06-11 12:32:05 +0900

    JS : Home contact Button

--- js/main.js
+++ js/main.js
@@ -14,11 +14,19 @@ document.addEventListener('scroll', () => {
 // Navbar menu 클릭 시 해당하는 위치로 Scrolling
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
+    const navbar_height = document.querySelector('#navbar').clientHeight;
     const target = event.target;
     const link = target.dataset.link;
     if (link == null) {
         return;
     }
-    const scrollTo = document.querySelector(link);
+    const scrollY = (document.querySelector(link).offsetTop) - navbar_height;
+    window.scrollTo({ top: scrollY, behavior: 'smooth' })
+});
+
+// "Contact me" Button Click Event
+const ContactMeBtn = document.querySelector('.home__contact');
+ContactMeBtn.addEventListener('click', (event) => {
+    const scrollTo = document.querySelector('#contact');
     scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
